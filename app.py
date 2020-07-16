928efaa2f37ffb659cecf765a4a8b3a221baa35a
Leonardo Felix <leonardofelixfelix@gmail.com>
2020-07-16 12:26:45 -0300

    Novo botão adicionado

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ from tkinter import *
 
 menu_inicial = Tk()
 menu_inicial.title('Pirata Enterprise LTDA')
-menu_inicial['bg'] = 'blue'
+menu_inicial['bg'] = 'gray'
 menu_inicial.geometry("500x250+200+200")
 menu_inicial.resizable(True,True)
 menu_inicial.minsize(500,250)
 menu_inicial.minsize(720,480)
 menu_inicial.iconbitmap("images/piratee.ico")
 
+def piraton_click(mensagem):
+    print(mensagem)
+
 #botão
-btn = Button(menu_inicial, text = "Executar")
+btn1 = Button(menu_inicial, text = "Executar", command=lambda: piraton_click('Nova mensagem'))
+btn.pack()
+
+btn2 = Button(menu_inicial, text = "Clicar", command=lambda: piraton_click('Outra mensagem'))
 btn.pack()
 
 
